14877ed445f0b21b59d77449850f62d7977f7bb5
Piyush990750 <119804070+Piyush990750@users.noreply.github.com>
2023-01-29 19:39:10 +0530

    Add files via upload

---
+++ Piyush_FOPJava_DSALabAlgo03/src/com/Piyush/DSA/PairSumInBST/Node.java
@@ -0,0 +1,15 @@
+package com.Piyush.DSA.PairSumInBST;
+
+
+public class Node {
+	int value;
+	Node left;
+	Node right;
+	Node(int data) {	
+	this.value=data;
+	this.left=null;
+	this.right=null;
+	
+	}
+		
+}
